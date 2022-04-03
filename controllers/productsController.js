943cc1a9e5d2ab01fe3d523879fb8eeeee542b8e
AlexDDks <magarciaa92@gmail.com>
2022-04-02 19:54:06 -0600

    mejorando formularios

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -22,23 +22,23 @@ const controller = {
         res.render("detail", { product })//se comparte la variable products con la vista detail
     },
     create: (req, res) => {
-        res.render('product-create-form')
+        // res.send('hola mundo')
+        // res.render('product-create-form')
+        res.render('createPrueba')
     },
 
     store: (req, res) => {
         // Do the magic
         const newProduct = {
-
-            //Al new body le faltan 2 campos de la base de datos, el id y la imagen por lo que vamos a ponerlos:
-            id: products[products.length - 1].id + 1, //el array de la base de datos tiene 8 elementos, nos queremos posicionar en el índice 7(el último del array) por lo tanto si el length es = 8, al restar 1 llegamos a la posición 7 del array que corresponde al último elemento
-
-            ...req.body //sintaxis spread nos permite tener todos los elementos del objeto body body.name, body.price... sin tener que escribir 1 por 1
-            //Así el new product va a tener todo lo del body
+            // id: 1,
+            // res.send(req.body.name)
+            name: req.body.name,
+            apellido: req.body.apellido
         }
         products.push(newProduct) //agrega al arreglo el producto que acabamos de insertar
 
         //     // express validator
-        fs.writeFileSync(productsFilePath, JSON.stringify(products, null, ' ')) //Esto es necesario pero no entendí para qué se usa
+        fs.writeFileSync(productsFilePath, JSON.stringify(products)) //Esto es necesario pero no entendí para qué se usa
         res.redirect("/products") //Redirige después de guarda un producto, se tiene que poner el path "completo"
     },
 
