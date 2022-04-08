66e97912db2de33d379852664c6749af2189885b
AlexDDks <magarciaa92@gmail.com>
2022-04-08 12:37:26 -0500

    problema en signup_2

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -21,15 +21,15 @@ const controller = {
         }
         users.push(newUser)
 
-        fs.writeFileSync(usersFilePath, JSON.stringify(users, null, ' '))
+        fs.writeFileSync(usersFilePath, JSON.stringify())
 
-        if (req.file == undefined) { //Acá para validar podemos poner varias opciones tipo "que sea menor en tamaño", "sea más grande que...etc". 
+        if (req.file != undefined) { //Acá para validar podemos poner varias opciones tipo "que sea menor en tamaño", "sea más grande que...etc". 
             //nos entrega todos los datos del file, sino se sube un file, entonces el resultado de este es undefined
             res.redirect("/users/login") //Redirige después de guarda un producto, se tiene que poner el path "completo"
 
         }
         else { //sino llega ningún file entonces recargamos de nuevo la página
-            res.rendirect('/users/signup')
+            res.render('signup')
         }
     }
 }
